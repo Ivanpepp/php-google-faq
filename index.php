c6028a4a12db773c54e3ce76896c7c9092077c3e
Ivanpepp <nastroivan3@gmail.com>
2021-10-13 14:30:54 +0200

    structure complete

---
+++ index.php
@@ -0,0 +1,124 @@
+<!-- 
+Ci sono diverse domande con relative risposte:
+Gestite il “Database” (i dati delle varie 
+domande e risposte) di queste domande e
+risposte con le strutture dati che abbiamo 
+visto insieme in PHP
+Occupatevi di gestire la 
+visualizzazione dei dati in PHP 
+-->
+
+<!DOCTYPE html>
+<?php 
+    $requests = array(
+        'faq1' => array(
+            'title' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+            'paragraph1' => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+            'paragraph2' => "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
+            'paragraph3' => "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
+            'paragraph4' => "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
+            'paragraph5' => "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
+        ),
+        'faq2' => array(
+            'title-faq2' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+            'paragraph1-faq2' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
+            'paragraph2-faq2' => "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
+            'paragraph3-faq2' => "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.",
+            'paragraph4-faq2' => "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
+            
+        ),
+        'faq3' => array(
+            'title-faq3' => "Perché il mio account è associato a un paese?",
+            'paragraph1-faq3' => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+            'paragraph1-underfaq-ol-1' => "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
+            'paragraph1a-underfaq-ol' => "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
+            'paragraph1b-underfaq-ol' => "Google LLC, con sede negli Stati Uniti, per il resto del mondo.",
+            'paragraph1-underfaq-ol-2' => "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
+            'paragraph2-faq3' => "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
+            'undertitle-faq3' => "Stabilire il paese associato al tuo account",
+            'undertitle-paragraph1-faq3' => "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.",
+            'undertitle-paragraph2-faq3' => "I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.",
+            'undertitle-paragraph3-faq3' => "Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.",  
+        ),
+        'faq4' => array(
+            'title-faq4' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+            'paragraph1-faq4' => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",        
+        ),
+        'faq5' => array(
+            'title-faq5' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+            'paragraph1-faq5' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",        
+        ),
+    )
+; ?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Google Faq</title>
+</head>
+<body>
+    
+    <main>
+        <?php foreach($requests as $faq1){ ?>
+        <h2><?php echo $faq1['title']; ?></h2>
+        <p><?php echo $faq1['paragraph1']; ?></p>
+        <p><?php echo $faq1['paragraph2']; ?></p>
+        <p><?php echo $faq1['paragraph3']; ?></p>
+        <p><?php echo $faq1['paragraph4']; ?></p>
+        <p><?php echo $faq1['paragraph5']; ?></p>
+        <?php }; ?>
+
+        <?php foreach($requests as $faq2){ ?>
+        <h2><?php echo $faq2['title-faq2']; ?></h2>
+        <p><?php echo $faq2['paragraph1-faq2']; ?></p>
+        <p><?php echo $faq2['paragraph2-faq2']; ?></p>
+        <p><?php echo $faq2['paragraph3-faq2']; ?></p>
+        <p><?php echo $faq2['paragraph4-faq2']; ?></p>
+        <?php }; ?>
+
+        <?php foreach($requests as $faq3){ ?>
+        <h2><?php echo $faq3['title-faq3']; ?></h2>
+        <p><?php echo $faq3['paragraph1-faq3']; ?></p>
+            <ol>
+                <li> <?php echo $faq3['paragraph1-underfaq-ol-1']; ?>
+                    <ol>
+                        <li>
+                            <?php echo $faq3['paragraph1a-underfaq-ol']; ?>
+                        </li>
+                        <li>
+                            <?php echo $faq3['paragraph1b-underfaq-ol']; ?>
+                        </li>
+                    </ol>
+                </li>
+                <li>
+                    <?php echo $faq3['paragraph1-underfaq-ol-2']; ?>
+                </li>
+            </ol>
+        <p><?php echo $faq3['paragraph2-faq3']; ?></p>
+        <h4>
+            <?php echo $faq3['undertitle-faq3']; ?>  
+        </h4>
+        <p><?php echo $faq3['undertitle-paragraph1-faq3']; ?></p>
+        <p><?php echo $faq3['undertitle-paragraph2-faq3']; ?></p>
+        <p><?php echo $faq3['undertitle-paragraph3-faq3']; ?></p>
+        <?php }; ?>
+        
+        <?php foreach($requests as $faq4){ ?>
+        <h2><?php echo $faq4['title-faq4']; ?></h2>
+        <p><?php echo $faq4['paragraph1-faq4']; ?></p>
+        <?php }; ?>
+
+        <?php foreach($requests as $faq5){ ?>
+        <h2><?php echo $faq5['title-faq5']; ?></h2>
+        <p><?php echo $faq5['paragraph1-faq5']; ?></p>
+        <?php }; ?>
+     
+    </main>
+
+
+</body>
+</html>
