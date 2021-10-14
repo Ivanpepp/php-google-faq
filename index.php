da13f771fb4ba388ca4330e00f7dd56901db2a51
Ivanpepp <nastroivan3@gmail.com>
2021-10-14 09:53:29 +0200

    foreach-problem-fixed

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ visualizzazione dei dati in PHP
     $requests = array(
         'faq1' => array(
             'title' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
-            'paragraph1' => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+            'paragraph' => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
 
             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
             
@@ -26,8 +26,8 @@ visualizzazione dei dati in PHP
            
         ),
         'faq2' => array(
-            'title-faq2' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
-            'paragraph1-faq2' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+            'title' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+            'paragraph' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
 
             Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
             
@@ -38,8 +38,8 @@ visualizzazione dei dati in PHP
             
         ),
         'faq3' => array(
-            'title-faq3' => "Perché il mio account è associato a un paese?",
-            'paragraph1-faq3' => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+            'title' => "Perché il mio account è associato a un paese?",
+            'paragraph' => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
 
             La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
                 Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
@@ -47,25 +47,26 @@ visualizzazione dei dati in PHP
             La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
         
         Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
-        ",
-            'undertitle-faq3' => "Stabilire il paese associato al tuo account",
-            'undertitle-paragraph1-faq3' => "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
+        <h4>Stabilire il paese associato al tuo account <?h4>
+        <p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
 
             I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
             
-            Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.",
+            Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato. </p>",
             
         ),
         'faq4' => array(
-            'title-faq4' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
-            'paragraph1-faq4' => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",        
+            'title' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+            'paragraph' => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.",        
         ),
         'faq5' => array(
-            'title-faq5' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
-            'paragraph1-faq5' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",        
+            'title' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+            'paragraph' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",        
         ),
     )
 ; ?>
+
+
 
 
 <html lang="en">
@@ -79,38 +80,9 @@ visualizzazione dei dati in PHP
 <body>
     
     <main>
-        <?php foreach($requests as $faq1){ ?>
-        <h2><?php echo $faq1['title']; ?></h2>
-        <p><?php echo $faq1['paragraph1']; ?></p>
-        <?php }; ?>
-
-        <?php foreach($requests as $faq2){ ?>
-        <h2><?php echo $faq2['title-faq2']; ?></h2>
-        <p><?php echo $faq2['paragraph1-faq2']; ?></p>
-        <?php }; ?>
-
-        <?php foreach($requests as $faq3){ ?>
-        <h2><?php echo $faq3['title-faq3']; ?></h2>
-        <p><?php echo $faq3['paragraph1-faq3']; ?></p>
-        <?php }; ?>
-      
-            <?php foreach($requests as $faq3){ ?>    
-        <p><?php echo $faq3['paragraph2-faq3']; ?></p>
-        <h4>
-            <?php echo $faq3['undertitle-faq3']; ?>  
-        </h4>
-        <p><?php echo $faq3['undertitle-paragraph1-faq3']; ?></p>
-      
-        <?php }; ?>
-        
-        <?php foreach($requests as $faq4){ ?>
-        <h2><?php echo $faq4['title-faq4']; ?></h2>
-        <p><?php echo $faq4['paragraph1-faq4']; ?></p>
-        <?php }; ?>
-
-        <?php foreach($requests as $faq5){ ?>
-        <h2><?php echo $faq5['title-faq5']; ?></h2>
-        <p><?php echo $faq5['paragraph1-faq5']; ?></p>
+        <?php foreach($requests as $faq){ ?>
+        <h2><?php echo $faq['title']; ?></h2>
+        <p><?php echo $faq['paragraph']; ?></p>
         <?php }; ?>
      
         
